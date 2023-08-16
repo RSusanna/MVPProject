382eaf3847a6944470a182872b857be810fa269e
koko6a <rus.baranovskiy@gmail.com>
2023-08-16 21:16:48 +0300

    initial commit

---
+++ MPVTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPVTestProject
+//
+//  Created by Кащенко on 16.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
